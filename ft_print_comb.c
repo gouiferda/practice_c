ae47d6ab72229f958f2015156b1d789ea32f7864
soufiane <soufiane.gouiferda@gmail.com>
2023-03-06 14:18:37 +0100

    ok

--- ft_print_comb.c
+++ ft_print_comb.c
@@ -8,15 +8,16 @@ void ft_print_comb(void){
 	int i = 0;
 	int num1 = 0;
 	int num2 = 0;
-	int num3 = 0;
-	int nb = 0;
-	while(i < 790){
-		nb = i;
-		num1 = (nb % 1000) / 100;
-		num2 = (nb % 100 ) / 10;	
-		num3 = nb % 10;
-		int cond1 = (num1 != num2 && num1 != num3 && num2 != num3);
-		int cond2 = (num1 < num2 && num2 < num3 && num1 < num3);
+	int num3 = 0; 
+	int cond1 = 0;
+	int cond2 = 0;
+
+	while(i < 790){ 
+		num1 = (i % 1000) / 100;
+		num2 = (i % 100 ) / 10;	
+		num3 = i % 10;
+		cond1 = (num1 != num2 && num1 != num3 && num2 != num3);
+		cond2 = (num1 < num2 && num2 < num3 && num1 < num3);
 		if (cond1 && cond2){
 			ft_putchar(num1 + '0');
 			ft_putchar(num2 + '0');
