f7edaa259a2a85ed239ebb1cc16ef079d5dbcd52
soufiane <soufiane.gouiferda@gmail.com>
2023-03-06 19:59:24 +0100

    ok

--- tools/make.sh
+++ tools/make.sh
@@ -1,8 +1,5 @@
 #!/bin/sh
 
-rm -rf out/*
-rm -rf obj/*
-rm -rf *.o
 gcc -O3 -w -Wall -Werror -Wextra -c -g src/mylib.c -o obj/mylib.o
 gcc -O3 -w -Wall -Werror -Wextra -c -g main.c obj/mylib.o -o obj/main.o
 gcc -O3 -w -Wall -Werror -Wextra -o out/app obj/main.o obj/mylib.o
